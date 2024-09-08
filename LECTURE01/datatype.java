12cd77a58208f6983f2509350379f4bd2451155d
ADBVCOE <abhidubey96503929@gmail.com>
2024-09-08 15:24:37 +0530

    second commit

--- LECTURE01/datatype.java
+++ LECTURE01/datatype.java
@@ -1,7 +1,7 @@
-package LECTURE00;
-public class Helloworld{
+package LECTURE01;
+
+public class datatype {
     public static void main(String[] args) {
-        System.out.println("hello");
         int a=20;
         System.out.print(a);
         System.out.println(a);
