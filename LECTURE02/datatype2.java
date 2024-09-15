809748c7902f32dc43ca8420fef746c3acbb0ae0
ADBVCOE <abhidubey96503929@gmail.com>
2024-09-15 10:09:58 +0530

    lecture02

---
+++ LECTURE02/datatype2.java
@@ -0,0 +1,31 @@
+package LECTURE02;
+
+public class datatype2 {
+    public static void main(String[] args) {
+    
+        int a=10;
+        int minINT=Integer.MIN_VALUE;
+        int maxINT=Integer.MAX_VALUE;
+        System.out.println(minINT);
+        System.out.println(maxINT);
+        short sh=-12;
+        Short minish=Short.MIN_VALUE;
+        Short maxish=Short.MAX_VALUE;
+        System.out.println(minish);
+        System.out.println(maxish);
+        float f=11.0f;
+        System.out.println(f);
+        double d=11.0123;
+        System.out.println(d);
+        char c='a';
+        System.out.println(c);
+        System.out.println(c+1);
+        byte b1=Byte.MIN_VALUE;
+        byte b2=Byte.MAX_VALUE;
+        System.out.println(b1);
+        System.out.println(b2);
+        
+    
+    }
+    
+}
---
+++ LECTURE02/operators.java
@@ -0,0 +1,12 @@
+package LECTURE02;
+
+public class operators {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a++ +" "+a++ + a);
+        System.out.println(a);
+
+        
+    }
+    
+}
